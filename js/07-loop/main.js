b19ad3144f964e063ecd081928ea5377a0dbfbfe
mijeong09 <me9809180@gmail.com>
2025-09-18 15:57:45 +0900

    loop statement

---
+++ js/07-loop/main.js
@@ -0,0 +1,31 @@
+// for(초기화문; 조건식; 증감문)
+for (let i = 0; i < 5; i++) {
+  console.log(`${i + 1}번 실행`);
+}
+
+// 반복문이 있는 코드
+for (let number1 = 0; number1 < 3; number1++) {
+  console.log(number1);
+}
+
+// 숫자를 0부터 5까지 출력
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+}
+
+// for 반복문
+// for(초기화문; 조건식; 증감문)
+// 반복 횟수가 명확 할 때
+
+// 0 부터 10까지 출력하는 반복문
+for (let i = 0; i < 11; i++) {
+  console.log(i);
+}
+
+// while 반복문
+// while (조건식)
+
+let j = 0; // 외부 초기화문
+while (j <= 10) {
+  // 내부 증감문
+}
