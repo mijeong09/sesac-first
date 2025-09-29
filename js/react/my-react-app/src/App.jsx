6c4ae40d4fff06e26dbc6b917a43808666b54921
mijeong09 <me9809180@gmail.com>
2025-09-29 13:07:49 +0900

    컴포넌트 이벤트 핸들링 onClick, onChangd

--- js/react/my-react-app/src/App.jsx
+++ js/react/my-react-app/src/App.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import Rule from "./components/JSX/Rule";
 import Interpolation from "./components/JSX/Interpolation";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MyButton from "./components/Component/MyButton";
-import MyList from "./components/Component/MyList";
+import OnClick from "./components/EventHandling/OnClick";
+import OnChange from "./components/EventHandling/OnChange";
 
 // App 컴포넌트는 Header 컴포넌트와 Interpolation 컴포넌트를 포함한다
 // App 컴포넌트는 Header 컴포넌트와 Interpolation 컴포넌트가 중첩됐다
+
+// App 컴포넌트 내부에 OnClick 컴포넌트 배치
 export default function App() {
   return (
     <div>
-      <Header></Header>
-      <Interpolation></Interpolation>
-      <MyList></MyList>
-      <MyButton></MyButton>
+      <OnClick></OnClick>
+      <OnChange></OnChange>
     </div>
+
+    // <div>
+    //   <Header></Header>
+    //   <Interpolation></Interpolation>
+    //   <MyList></MyList>
+    //   <MyButton></MyButton>
+    // </div>
   );
 }
---
+++ js/react/my-react-app/src/components/EventHandling/OnChange.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function OnChange() {
+  // change 이벤트의 핸들러 함수 정의
+  // 핸들러 함수 이름 규칙: handle이벤트명
+
+  // 이벤트 핸들러의 event 객체 : 이벤트 정보를 저장한 객체
+  function handleChange(event) {
+    console.log(event);
+    // 이벤트가 발생한 요소의 정보
+    console.log(event["target"]);
+    // 이벤트가 발생한 입력 요소의 사용자 입력 값
+    console.log(event["target"]["value"]);
+  }
+
+  // 실습 1.
+  function handleNumberChange(event) {
+    // 이벤트가 발생한 입력 요소의 사용자 입력 값이 10보다 작으면
+    // console.log()를 사용해서 "10보다 작은 수" 라고 출력
+    const value = event["target"]["value"];
+    if (value < 10) {
+      console.log("10보다 작은 값");
+    }
+  }
+
+  return (
+    <div>
+      {/* 실습2 */}
+      {/* onChange 이벤트 속성에서 이벤트 객체를 전달하면서 handleNumberChange 호출 */}
+      <input
+        type="number"
+        className="w-7xl border-2 border-gray-700 p-2 m-2"
+        onChange={(event) => {
+          handleNumberChange(event);
+        }}
+      />
+
+      {/* onChange 이벤트 속성 적용 */}
+      <input
+        type="text"
+        // 이벤트 객체 event는 화살표 함수의 매개변수
+        onChange={(event) => {
+          // handleChange() 함수 호출
+          // 화살표 함수의 매개변수 event에서 가져옴
+          handleChange(event);
+        }}
+        className="w-7xl border-2 border-gray-700 p-2 m-2"
+      />
+    </div>
+  );
+}
