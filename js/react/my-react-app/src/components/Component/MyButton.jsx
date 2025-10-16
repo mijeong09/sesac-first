c4ecc89d24c6c48d76c1c0db5a07d0a3e3c5150c
mijeong09 <me9809180@gmail.com>
2025-10-16 14:47:20 +0900

    react 디렉토리 이동

--- js/react/my-react-app/src/components/Component/MyButton.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function MyButton() {
-  return <button>버튼</button>;
-}
--- js/react/my-react-app/src/components/TMDB/MovieSearch.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MovieSearch() {
-  return (
-    <div>MovieSearch</div>
-  )
-}
