b9bbe21d2edf530c2eacb592574f306777d5bd90
mijeong09 <me9809180@gmail.com>
2025-10-17 09:16:54 +0900

    MessageList.jsx 주석 추가

--- react/chat-bot-app/src/components/MessageList.jsx
+++ react/chat-bot-app/src/components/MessageList.jsx
@@ -3,10 +3,12 @@ import ChatMessage from "./ChatMessage";
 
 // Props messages : 사용자 / AI 메세지 저장 배열
 export default function MessageList({ messages }) {
+  // useRef 훅 : document.querySelector() -> 요소를 선택
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
-    // 응답 메시지가 추가되면 최하단으로 스크롤
+    // 응답 메시지가 추가되면(변경될 때 마다) 최하단으로 스크롤
+    // messagesEndRef가 가르키는 요소로 스크롤을 이동
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
@@ -16,6 +18,7 @@ export default function MessageList({ messages }) {
         <ChatMessage key={index} message={message} />
       ))}
       {/* 하단 스크롤 유지를 위한 빈 div */}
+      {/* 모든 채팅 내역 하단에 위치하는 div */}
       <div ref={messagesEndRef}></div>
     </div>
   );
